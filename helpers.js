ed0a3d52b22cede08beaddddf9904c136c2fcdf1
Ndumiso19894 <135328564+Ndumiso19894@users.noreply.github.com>
2025-11-25 02:03:01 +0200

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,10 @@
+export function prepareHeaders() {
+  return {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125 Safari/537.36",
+    "Accept": "text/html",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache"
+  };
+}
